1207ba159492ba7eecd9b1a3f403c30ed36efe12
Dalton Tofoli <53830399+dtofoli1@users.noreply.github.com>
2020-01-26 01:41:59 -0300

    New tests
    Enzyme e Jest

--- site/src/components/spec.test.js
+++ site/src/components/spec.test.js
@@ -2,6 +2,11 @@ import React from 'react'
 import shallow  from 'enzyme'
 import Share from './share.js'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme'
+
+Enzyme.configure({
+    adapter: new EnzymeAdapter()
+})
 
 describe('Share Component', () => {
     it('It should render without errors', () => {
